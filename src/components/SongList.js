b9011bece3c5c0fa859576118c4c8b635142ff40
Fofiko <foteinid@gmail>
2018-10-31 09:38:54 +0000

    Change chart position assignment to index

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import SongItem from './SongItem'
 
 const SongList = ({songs}) => {
-    const songNodes = songs.map(song => {
+    const songNodes = songs.map((song, index) => {
         return (<SongItem
             key={song.id.attributes['im:id']}
-            position={song.category.attributes['im:id']}
+            position={index +1}
             title={song['im:name'].label}
             artist={song['im:artist'].label}
             />)
